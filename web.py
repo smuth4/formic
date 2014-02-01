dc04238e3c0f797b3a3754b7fdebd0ce8b96191e
smuth4 <smuth4@gmail.com>
2014-02-02 00:42:55 +0300

    Moved debug to ini, add url trailing slash flexibility

--- web.py
+++ web.py
@@ -9,19 +9,19 @@ from ansible import callbacks
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/inventories")
+@app.route("/inventories/")
 def index():
     return render_template('index.html', inventories=engine.listInventoryFiles())
 
-@app.route("/hosts")
+@app.route("/hosts/")
 def hosts():
     return render_template('hosts.html', hosts=engine.listHosts(groups=True))
 
-@app.route("/playbooks")
+@app.route("/playbooks/")
 def playbooks():
     return render_template('playbooks.html', playbooks=engine.listPlaybooks())
 
-@app.route("/playbooks/run")
+@app.route("/playbooks/run/")
 def runPlaybook():
     return render_template('playbooks_run.html',
                            playbooks=engine.listPlaybooks(),
@@ -29,18 +29,18 @@ def runPlaybook():
                            default_p=request.args.get('p', ''),
                            default_i=request.args.get('i', ''))
 
-@app.route("/playbooks/launch", methods=['POST'])
+@app.route("/playbooks/launch/", methods=['POST'])
 def launchPlaybook():
     if request.method == "POST":
         engine.runPlaybook(request.form['inventory'], request.form['playbook'])
         return redirect(url_for('watchPlaybook'))
     return redirect(url_for('runPlaybook'))
 
-@app.route("/playbooks/watch")
+@app.route("/playbooks/watch/")
 def watchPlaybook():
     return render_template('playbooks_watch.html')
 
-@app.route("/playbooks/run/status")
+@app.route("/playbooks/run/status/")
 def runningPlaybookStatus():
     return jsonify(engine.getStatus())
 
@@ -49,16 +49,15 @@ def getConfig():
     config_defaults = {
         "Bind": "127.0.0.1",
         "Base": os.path.dirname(os.path.realpath(__file__)),
+        "Debug": False,
         }
+    global config
     config = ConfigParser.SafeConfigParser(config_defaults)
     config.read('formic.ini')
     
     base = config.get('Ansible', 'Base') 
-    global bind
-    bind = config.get('Web', 'Bind')
-    
     engine = ansibleengine.AnsibleEngine(base)
     
 if __name__ == "__main__":
     getConfig()
-    app.run(host=bind, debug=True)
+    app.run(host=config.get('Web', 'Bind'), debug=config.get('Web', 'Debug'))
